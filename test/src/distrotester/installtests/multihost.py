49827a88cf02600f8e3741b208a939037f1084b3
Aaron Kimball <aaron@cloudera.com>
2009-02-26 20:35:58 -0800

    incremental checkin of distrotester refactoring
    CH-79: Factored out common multihost/standalone code into installbase.py
    CH-90: Added redhatcommon setup module with fedora8 and centos5 as subclasses
    refactoring distro tests to use RPMs

--- test/src/distrotester/installtests/multihost.py
+++ test/src/distrotester/installtests/multihost.py
@@ -16,6 +16,7 @@ from   com.cloudera.testutil.verbosetest import VerboseTestCase
 import com.cloudera.tools.shell as shell
 
 from   distrotester.constants import *
+import distrotester.installtests.installbase as installbase
 import distrotester.testproperties as testproperties
 from   distrotester.functiontests.hadooptests import HadoopTest
 from   distrotester.functiontests.hivetests import HiveTest
@@ -24,145 +25,18 @@ from   distrotester.functiontests.pigtests import PigTest
 from   distrotester.functiontests.scribetests import ScribeTest
 import distrotester.functiontests.secondarynamenodetests as secondarynamenodetests
 
-class MultiHostTest(VerboseTestCase):
+class MultiHostTest(installbase.InstallBaseTest):
 
   def __init__(self, methodName='runTest'):
-    VerboseTestCase.__init__(self, methodName)
-    self.curHadoopSite = None
-
-    # Get our hostname and memoize it
-    self.hostname = socket.getfqdn()
-
-
-  def getPlatformSetup(self):
-    """ Get the PlatformSetup object used to initialize the node """
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-
-    # delaying this import til this thunk is used to avoid
-    # circular dependency
-    import distrotester.platforms as platforms
-
-    properties = self.getProperties()
-    platformName = properties.getProperty(TEST_PLATFORM_KEY)
-    return platforms.setupForPlatform(platformName, properties)
-
-
-  def getHadoopDir(self):
-    return os.path.join(INSTALL_PREFIX, "hadoop")
-
-  def getHadoopCmd(self):
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-    return os.path.join(self.getHadoopDir(), "bin/hadoop")
-
-  def getSlavesFile(self):
-    """ Return the slaves file provided by the remote test manager """
-    return self.getProperties().getProperty(SLAVES_FILE_KEY)
-
-  def getSlavesList(self):
-    slavesFile = self.getSlavesFile()
-    try:
-      handle = open(slavesFile)
-      lines = handle.readlines()
-      slavesList = []
-      for line in lines:
-        slavesList.append(line.strip())
-      handle.close()
-      return slavesList
-    except IOError, ioe:
-      logging.error("Error opening slaves file: " + str(slavesFile))
-      logging.error(ioe)
-      return []
-
-
-  def getProperties(self):
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-    return testproperties.getProperties()
-
-  def prepHadoopSite(self, inputHadoopSite):
-    """ given an input hadoop-site.xml file we want to use, we must
-        first replace the 'MASTER_HOST' string with the current
-        hostname.
-    """
-
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-
-    # Get a temporary filename to use as the hadoop-site.xml file.
-    (oshandle, tmpFilename) = tempfile.mkstemp()
-    self.curHadoopSite = tmpFilename
-    try:
-      handle = os.fdopen(oshandle, "w")
-      handle.close()
-    except OSError:
-      # irrelevant
-      pass
-    except IOError:
-      # irrelevant
-      pass
-
-    # put the hadoop site file through a sed script.
-    script = 'sed -e "s/MASTER_HOST/' + self.hostname + '/" ' \
-        + inputHadoopSite + " > " + self.curHadoopSite
-    shell.sh(script)
-
-    return self.curHadoopSite
-
-  def stopHadoopForUser(self, user):
-    """ Stop the hadoop daemons run by a given hadoop username """
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-    try:
-      # stop any hadoop run as hadoop user.
-      hadoopDir = self.getHadoopDir()
-      stopCmd = os.path.join(hadoopDir, "bin/stop-all.sh")
-      shell.sh("sudo -H -u " + user + " " + stopCmd)
-    except shell.CommandError, ce:
-      pass # nothing to shut down? ok
-
-  def stopHadoop(self):
-    # TODO(aaron): Refactor this out into common base class for
-    # multihost and standalone (CH-77)
-    self.stopHadoopForUser(ROOT_USER)
-    self.stopHadoopForUser(HADOOP_USER)
-
+    installbase.InstallBaseTest.__init__(self, methodName)
 
   def tearDown(self):
-    # TODO(aaron): This code block is common to MultiHost and Standalone
-    # (CH-77)
-    if self.curHadoopSite != None:
-      # remove this temp file we created
-      os.remove(self.curHadoopSite)
-
-    # self.stopHadoop()
-    VerboseTestCase.tearDown(self)
-
-
-  def doSshAll(self, cmd):
-    " Execute a command on all hosts. "
-
-    logging.debug("sshall command: " + cmd)
-
-    allHosts = self.getSlavesList()
-    allHosts.append(self.hostname)
-
-    results = sshall.sshMultiHosts("root", allHosts, cmd, \
-        self.getProperties(), SSH_RETRIES, SSH_PARALLEL)
-    for result in results:
-      # each result is an sshall.SshResult obj
-      if result.getStatus() != 0:
-        logging.error("Got error status executing command on " \
-            + result.getHost())
-        logging.error("Output:")
-        for line in result.getOutput():
-          logging.error("  " + line.rstrip())
+    installbase.InstallBaseTest.tearDown()
 
   def setUp(self):
     """ shutdown and remove existing hadoop distribution. """
 
-    VerboseTestCase.setUp(self)
+    installbase.InstallBaseTest.setUp(self)
 
     try:
       self.stopHadoop()
@@ -175,12 +49,6 @@ class MultiHostTest(VerboseTestCase):
     # Do this on all hosts.
 
     self.doSshAll("killall scribed")
-    self.doSshAll("rm -rf " + BASE_TMP_DIR)
-    self.doSshAll("rm -rf " + INSTALL_PREFIX)
-    self.doSshAll("rm -rf " + CONFIG_PREFIX)
-    self.doSshAll("mkdir -p " + BASE_TMP_DIR)
-    self.doSshAll("chmod a+w " + BASE_TMP_DIR)
-    self.doSshAll("chmod o+t " + BASE_TMP_DIR)
 
     # Delete things associated with scribe on all hosts
     self.doSshAll("rm -rf " + SCRIBE_OUT_DIR)
@@ -189,131 +57,58 @@ class MultiHostTest(VerboseTestCase):
     shell.sh("rm -rf " + LOGMOVER_OUT_DIR)
 
 
-  def testHadoopOnly(self):
-    """ Install *only* the hadoop component.
-        Install, run, and use hadoop as root
-    """
+  def start_hadoop(self):
+    shell.sh("service hadoop-namenode start")
+    shell.sh("service hadoop-jobtracker start")
 
-    javaHome = self.getProperties().getProperty(JAVA_HOME_KEY)
+    # Do this on slaves only
+    self.doSshAll("service hadoop-datanode start", True)
+    self.doSshAll("service hadoop-tasktracker start", True)
 
-    cmd = INSTALLER_COMMAND + " --unattend --prefix " + INSTALL_PREFIX \
-        + " --role jobtracker,namenode,secondary_namenode,deployment_master" \
-        + " --config-prefix " + CONFIG_PREFIX \
-        + " --log-filename " + INSTALLER_LOG_FILE \
-        + " --format-hdfs --hadoop-user root " \
-        + " --java-home " + javaHome \
-        + " --hadoop-slaves " + self.getSlavesFile() \
-        + " --identity /root/.ssh/id_rsa" \
-        + " --hadoop-site " \
-        + self.prepHadoopSite("hadoop-configs/basic-config.xml") \
-        + " --debug"
-    logging.debug("Installing with command: " + cmd)
-    shell.sh(cmd)
-
-    self.getProperties().setProperty(HADOOP_USER_KEY, ROOT_USER)
-    self.getProperties().setProperty(CLIENT_USER_KEY, ROOT_USER)
-
-    hadoopSuite = unittest.makeSuite(HadoopTest, 'test')
-    functionalityTests = unittest.TestSuite([
-        hadoopSuite
-        ])
-
-    print "Running Hadoop functionality tests"
-    runner = unittest.TextTestRunner()
-    if not runner.run(functionalityTests).wasSuccessful():
-      self.fail()
-
-  def testCreateSshKeys(self):
-    """ Removes ~hadoop/.ssh/id_rsa; attempts to ensure that this can still
-        be regenerated by the installer.
-        Then install *only* the hadoop component.
-        Run in multi-user mode.
-    """
-
-    javaHome = self.getProperties().getProperty(JAVA_HOME_KEY)
-
-    logging.debug("Initial ssh key md5:")
-    id_rsa_md5 = shell.shLines("md5sum /home/hadoop/.ssh/id_rsa")
-    logging.debug(id_rsa_md5)
-    logging.debug("Removing id_rsa file for hadoop user")
-    os.rename("/home/hadoop/.ssh/id_rsa", "/home/hadoop/.ssh/id_rsa_backup")
-
-    try:
-      cmd = INSTALLER_COMMAND + " --unattend --prefix " + INSTALL_PREFIX \
-          + " --role jobtracker,namenode,secondary_namenode,deployment_master" \
-          + " --config-prefix " + CONFIG_PREFIX \
-          + " --log-filename " + INSTALLER_LOG_FILE \
-          + " --format-hdfs --hadoop-user " + HADOOP_USER \
-          + " --java-home " + javaHome \
-          + " --hadoop-slaves " + self.getSlavesFile() \
-          + " --identity /root/.ssh/id_rsa" \
-          + " --create-keys" \
-          + " --hadoop-site " \
-          + self.prepHadoopSite("hadoop-configs/basic-config.xml") \
-          + " --debug"
-      logging.debug("Installing with command: " + cmd)
-      shell.sh(cmd)
-
-      self.getProperties().setProperty(HADOOP_USER_KEY, HADOOP_USER)
-      self.getProperties().setProperty(CLIENT_USER_KEY, CLIENT_USER)
-
-      hadoopSuite = unittest.makeSuite(HadoopTest, 'test')
-      functionalityTests = unittest.TestSuite([
-          hadoopSuite
-          ])
-
-      print "Running Hadoop functionality tests"
-      runner = unittest.TextTestRunner()
-      if not runner.run(functionalityTests).wasSuccessful():
-        self.fail()
-    finally:
-      # Restore the original id_rsa file on our way out.
-      # Also, delete the id_rsa.pub.cloudera file that we leave behind.
-      os.rename("/home/hadoop/.ssh/id_rsa_backup", "/home/hadoop/.ssh/id_rsa")
-      os.remove("/home/hadoop/.ssh/id_rsa.pub.cloudera")
-      logging.debug("Final ssh key md5:")
-      id_rsa_md5 = shell.shLines("md5sum /home/hadoop/.ssh/id_rsa")
-      logging.debug(id_rsa_md5)
+    # TODO(aaron): Start 2nn on the appropriate host
 
 
-  def testAllApps(self):
+  def test_all_apps(self):
     """ Install all components.
         Use a separate hadoop user account and a separate client account. """
 
-    javaHome = self.getProperties().getProperty(JAVA_HOME_KEY)
+    platform_setup = self.getPlatformSetup()
 
-    cmd = INSTALLER_COMMAND + " --unattend --prefix " + INSTALL_PREFIX \
-        + " --role master,developer" \
-        + " --config-prefix " + CONFIG_PREFIX \
-        + " --log-filename " + INSTALLER_LOG_FILE \
-        + " --format-hdfs --hadoop-user " + HADOOP_USER \
-        + " --java-home " + javaHome \
-        + " --hadoop-slaves " + self.getSlavesFile() \
-        + " --identity /root/.ssh/id_rsa" \
-        + " --hadoop-site " \
-        + self.prepHadoopSite("hadoop-configs/basic-config.xml") \
-        + ' --namenode "' + self.hostname + ':9000" ' \
-        + ' --jobtracker "' + self.hostname + ':9001"' \
-        + " --overwrite-htdocs" \
-        + " --debug"
+    # install basic configuration
+    platform_setup.installPackage("hadoop")
 
-    logging.debug("Installing with command: " + cmd)
-    shell.sh(cmd)
+    # TODO(aaron): Install configuration and enable via alternatives
+
+    # install virtual packages for services
+    platform_setup.installPackage("hadoop-namenode")
+    platform_setup.installPackage("hadoop-secondarynamenode")
+    platform_setup.installPackage("hadoop-jobtracker")
+
+    # install additional bundled applications
+    platform_setup.installPackage("hadoop-pig")
+
+    # TODO(aaron): Test MRUnit here.
+    # platform_setup.installPackage("mrunit")
+
+    # TODO(aaron): Install packages on slave nodes
+
+    self.start_hadoop()
 
     self.getProperties().setProperty(HADOOP_USER_KEY, HADOOP_USER)
     self.getProperties().setProperty(CLIENT_USER_KEY, CLIENT_USER)
 
+    # TODO(aaron): Enable tests as they're available
     hadoopSuite   = unittest.makeSuite(HadoopTest, 'test')
-    hiveSuite     = unittest.makeSuite(HiveTest, 'test')
-    logMoverSuite = unittest.makeSuite(LogMoverTest, 'test')
+#    hiveSuite     = unittest.makeSuite(HiveTest, 'test')
+#    logMoverSuite = unittest.makeSuite(LogMoverTest, 'test')
     pigSuite      = unittest.makeSuite(PigTest, 'test')
-    scribeSuite   = unittest.makeSuite(ScribeTest, 'test')
+#    scribeSuite   = unittest.makeSuite(ScribeTest, 'test')
     functionalityTests = unittest.TestSuite([
         hadoopSuite,
-        hiveSuite,
-        logMoverSuite,
+#        hiveSuite,
+#        logMoverSuite,
         pigSuite,
-        scribeSuite
+#        scribeSuite
         ])
 
     print "Running Hadoop functionality tests"
@@ -322,83 +117,18 @@ class MultiHostTest(VerboseTestCase):
       self.fail()
 
 
-  def testWithHostsFiles(self):
-    """ All apps, separate accounts, with dfs.hosts and dfs.hosts.exclude
-        files set in place.
-    """
 
-    javaHome = self.getProperties().getProperty(JAVA_HOME_KEY)
-
-    cmd = INSTALLER_COMMAND + " --unattend --prefix " + INSTALL_PREFIX \
-        + " --role master,developer" \
-        + " --config-prefix " + CONFIG_PREFIX \
-        + " --log-filename " + INSTALLER_LOG_FILE \
-        + " --format-hdfs --hadoop-user " + HADOOP_USER \
-        + " --java-home " + javaHome \
-        + " --hadoop-slaves " + self.getSlavesFile() \
-        + " --identity /root/.ssh/id_rsa" \
-        + " --make-dfs-hosts dfs.hosts" \
-        + " --make-dfs-excludes dfs.hosts.exclude" \
-        + " --hadoop-site " \
-        + self.prepHadoopSite("hadoop-configs/hosts-config.xml") \
-        + ' --namenode "' + self.hostname + ':9000" ' \
-        + ' --jobtracker "' + self.hostname + ':9001"' \
-        + " --overwrite-htdocs" \
-        + " --debug"
-
-    shell.sh(cmd)
-
-    self.getProperties().setProperty(HADOOP_USER_KEY, HADOOP_USER)
-    self.getProperties().setProperty(CLIENT_USER_KEY, CLIENT_USER)
-
-    hadoopSuite = unittest.makeSuite(HadoopTest, 'test')
-    hiveSuite   = unittest.makeSuite(HiveTest, 'test')
-    pigSuite    = unittest.makeSuite(PigTest, 'test')
-    scribeSuite = unittest.makeSuite(ScribeTest, 'test')
-    functionalityTests = unittest.TestSuite([
-        hadoopSuite,
-        hiveSuite,
-        pigSuite,
-        scribeSuite
-        ])
-
-    print "Running Hadoop functionality tests"
-    runner = unittest.TextTestRunner()
-    if not runner.run(functionalityTests).wasSuccessful():
-      self.fail()
-    pass
-
-  def testGuidedInstall(self):
-    """ Use stdin to handle guided installation. """
-    # TODO: This (CH-79)
-    # TODO: set the editor to /bin/true, provide a slaves file.
-    pass
-
-
-  def testDoubleInstall(self):
-    """ Run the installer 2x in a row then test """
-
-    # This tests CH-118 - "installer should stop scribe, hadoop daemons"
-
-    logging.info("Performing first install/test in repeated batch.")
-    self.testAllApps()
-    # the second installation will reformat the hdfs instance, but
-    # we need to manually destroy the hdfs data dir first or else
-    # it will fail to boot (bad namespace).
-    self.doSshAll("rm -rf /mnt/tmp/data")
-    logging.info("Performing second install/test in repeated batch.")
-    self.testAllApps()
-
-
-  def testSeparateSecondary(self):
+  def test_separate_secondary(self):
     """ Test separate secondarynamenode.
         Install all components.
         Put the NN and JT on one machine.
         Put the 2NN on the other machine, along with DN and TT.
         Use a separate hadoop user account and a separate client account. """
 
+
+    # TODO(aaron): Rewrite this entire method.
+
     properties = self.getProperties()
-    javaHome = properties.getProperty(JAVA_HOME_KEY)
 
     # Use the first slave as the 2nn.
     slavesList = self.getSlavesList()
--- test/src/distrotester/setup/centos5.py
+++ test/src/distrotester/setup/centos5.py
@@ -11,9 +11,10 @@ import com.cloudera.tools.shell as shell
 
 from   distrotester.constants import *
 from   distrotester.setup.platformsetup import PlatformSetup
+import distrotester.setup.redhatcommon as redhatcommon
 
 
-class Centos5Setup(PlatformSetup):
+class Centos5Setup(redhatcommon.RedHatCommonSetup):
   """ Performs setup of packages on a CentOS 5.2 installation.
       Works on both i386 and x86_64 architectures; one of the
       strings "i386" or "x86_64" must be provided as the 'arch'
@@ -21,17 +22,11 @@ class Centos5Setup(PlatformSetup):
   """
 
   def __init__(self, arch, properties):
-    PlatformSetup.__init__(self, properties)
-    self.arch = arch
-
-    self.bucket = properties.getProperty(PACKAGE_BUCKET_KEY, \
-        PACKAGE_BUCKET_DEFAULT)
+    RedHatCommonSetup.__init__(self, arch, properties)
 
 
   def remoteBootstrap(self):
-    """ Remote bootstrap for Fedora 5 """
-
-    # TODO(aaron): Need python25 for CentOS.
+    """ Remote bootstrap for CentOS 5 """
 
     # Need to make sure we can use python!
     # Also, ruby for s3cmd
@@ -39,126 +34,12 @@ class Centos5Setup(PlatformSetup):
       "yum -y install python python-devel ruby rsync"
     ]
 
-  def initProperties(self):
-    """ Set any properties specific to this platform """
-
-    # set the java home location
-    if self.arch == "x86_64":
-      self.properties.setProperty(JAVA_HOME_KEY, "/usr/java/jdk1.6.0_07")
-    elif self.arch == "i386":
-      self.properties.setProperty(JAVA_HOME_KEY, "/usr/java/jdk1.6.0_10")
-
-  def writeSshConfig(self, handle):
-    """ write an ssh config file for unattended use to the
-        open file handle """
-
-    handle.write("""
-Host *
-  StrictHostKeyChecking no
-  PasswordAuthentication no
-  GSSAPIAuthentication no
-  NoHostAuthenticationForLocalhost yes
-  ServerAliveInterval 60
-  ServerAliveCountMax 60
-""")
-
-
-
-  def makeUser(self, username, rootSshKeys=True):
-    """ Make a user account on the machine. if rootSshKeys is true,
-        copy the authorized_keys and id_rsa files from /root/.ssh/ """
-
-    try:
-      # if we can get the user's passwd entry, we don't need to make it
-      shell.sh("getent passwd " + username)
-    except shell.CommandError, ce:
-      # we do.
-      shell.sh("useradd --create-home " + username)
-
-    shell.sh("mkdir -p /home/" + username + "/.ssh")
-    shell.sh("chmod 0755 /home/" + username)
-    shell.sh("chmod 0750 /home/" + username + "/.ssh")
-
-    if rootSshKeys:
-      shell.sh("cp /root/.ssh/authorized_keys /home/" + username + "/.ssh")
-      shell.sh("chmod 0600 /home/" + username + "/.ssh/authorized_keys")
-      if os.path.exists("/root/.ssh/id_rsa"):
-        shell.sh("cp /root/.ssh/id_rsa /home/" + username + "/.ssh")
-        shell.sh("chmod 0600 /home/" + username + "/.ssh/id_rsa")
-
-    shell.sh("chown -R " + username + ":" + username \
-        +" /home/" + username)
-
-    handle = open("/home/" + username + "/.ssh/config", "w")
-    self.writeSshConfig(handle)
-    handle.close()
-
-    shell.sh("chown " + username + ":" + username \
-        +" -R /home/" + username + "/.ssh")
-
-
-  def removePackage(self, package):
-    shell.sh("yum -y remove " + package)
-
-  def installPackage(self, package):
-    shell.sh("yum -y install " + package)
 
   def setup(self):
     """ Install on CentOS 5.2. """
 
-    # We download java with s3cmd. We need that first.
-    s3dest = os.path.join(PACKAGE_TARGET, S3CMD_PACKAGE_NAME)
-    self.wget(S3CMD_URL, s3dest)
-
-    self.untar(s3dest, PACKAGE_TARGET)
-
-    s3destDir = os.path.join(PACKAGE_TARGET, "s3sync/*.rb")
-    shell.sh("cp " + s3destDir + " /usr/bin")
-
-    # write out the s3 environment
-    access_key_id = self.properties.getProperty(AWS_ACCESS_KEY_ID)
-    secret_access_key = self.properties.getProperty(AWS_SECRET_ACCESS_KEY)
-
-    shell.sh("mkdir -p /etc/s3conf")
-
-    handle = open("/etc/s3conf/s3config.yml", "w")
-    handle.write("""
-aws_access_key_id: %(access)s
-aws_secret_access_key: %(secret)s
-""" % { "access" : access_key_id,
-        "secret" : secret_access_key })
-    handle.close()
-
-    # now install Java.
-    if self.arch == "x86_64":
-      jdkPackage = "jdk-6u7-linux-" + self.arch + "-rpm.bin"
-    elif self.arch == "i386":
-      jdkPackage = "jdk-6u10-linux-" + self.arch + "-rpm.bin"
-
-    jdkPath = self.properties.getProperty(JAVA_HOME_KEY)
-    jdkPackageDest = os.path.join(PACKAGE_TARGET, jdkPackage)
-
-    self.s3get(self.bucket, "packages/" + jdkPackage, jdkPackageDest)
-    shell.sh("yum -y install yum-allowdowngrade")
-    shell.sh("yum -y remove \"java-*-icedtea\" \"java-*-icedtea-devel\" " \
-        + "\"java-*-icedtea-plugin\"")
-    shell.sh("yum -y install compat-libstdc++-33 compat-libstdc++-296")
-    shell.sh("chmod 0700 \"" + jdkPackageDest + "\"")
-
-    # do the "more hack" to deny Java the right to have user intervention.
-    # We accept the license; we don't need to have someone sit here and type
-    # it in every time.
-    shell.sh("mv /bin/more /bin/no.more")
-
-    # Actually install Java!
-    shell.sh("yes | " + jdkPackageDest + " -noregister")
-
-    handle = open("/etc/profile", "a")
-    handle.write("\nexport JAVA_HOME=" + jdkPath + "\n")
-    handle.close()
-
-    # restore 'more' to where it should be.
-    shell.sh("mv /bin/no.more /bin/more")
+    # perform common setup steps we share with other RH-based distributions
+    self.redhat_common_setup()
 
     # install python 2.5 to run the rest of our code.
     python_25_package = "python-25-2.5.1-25.i386.rpm"
@@ -176,22 +57,5 @@ aws_secret_access_key: %(secret)s
       shell.sh("yum -y remove curl.i386")
       shell.sh("yum -y install curl.x86_64")
 
-    # configure ssh so that it doesn't raise a fuss about unknown hosts.
-    handle = open("/root/.ssh/config", "w")
-    self.writeSshConfig(handle)
-    handle.close()
-
-    self.makeUser(CLIENT_USER)
-
-    # Change the sudoers file to preserve environment variables we
-    # care a lot about.
-    # TODO(aaron): If you add an environment variable to the install plan,
-    # you have to add it here. This is unfortunate; we shouldn't need to
-    # list all this twice. (CH-80)
-    handle = open("/etc/sudoers", "a")
-    handle.write("""
-Defaults  env_keep += "JAVA_HOME HADOOPDIR HADOOP_HOME PIGDIR PIG_CLASSPATH"
-""")
-    handle.close()
 
 
