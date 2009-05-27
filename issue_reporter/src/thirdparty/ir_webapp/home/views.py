5f3fc9548f615606535c638aaea39980769d4537
Matt Massie <matt@cloudera.com>
2009-05-27 14:41:11 -0700

    Removing tools we no longer use

--- issue_reporter/src/thirdparty/ir_webapp/home/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponsePermanentRedirect
-
-def index(request):
-  """
-  Redirect requests to the issues web app
-  """
-  return HttpResponsePermanentRedirect('/issues/')
