ffd3813d8a924bcdafa4d53c48eed274fc13b4a9
Matt Massie <matt@cloudera.com>
2009-07-29 11:29:53 -0700

    Cleanup the GITPACKAGE targets and add very basic help

--- Makefile
+++ Makefile
@@ -1,35 +1,46 @@
-BASE_DIR:=$(shell pwd)
-BUILD_DIR?=$(BASE_DIR)/build
-DL_DIR   ?=$(BASE_DIR)/dl
-STAMP_DIR:=$(BUILD_DIR)/stamps
+BASE_DIR  :=$(shell pwd)
+BUILD_DIR ?=$(BASE_DIR)/build
+DL_DIR    ?=$(BASE_DIR)/dl
+STAMP_DIR :=$(BUILD_DIR)/stamps
+OUTPUT_DIR?=$(BUILD_DIR)/output
 
--include config.mk
-include mk/gitpackage.mk
+REQUIRED_DIRS = $(BUILD_DIR) $(DL_DIR) $(STAMP_DIR) $(OUTPUT_DIR)
+$(shell for d in $(REQUIRED_DIRS); \
+  do                               \
+    [[ -d $$d ]] || mkdir -p $$d;  \
+  done) 
 
 TARGETS:=
+TARGETS_HELP:=
 
-all: build-dirs packages
+# optional configuration variables
+-include config.mk
 
-build-dirs:
-	-@mkdir -p $(BUILD_DIR) $(DL_DIR) $(STAMP_DIR)
+include mk/gitpackage.mk
+
+help: package-help
+
+all: packages
+world: all
 
 # Hadoop 0.18.3-based hadoop package
-HADOOP18_SOURCE=hadoop-0.18.3.tar.gz
-HADOOP18_SITE=http://apache.cloudera.com/hadoop/core/hadoop-0.18.3
-HADOOP18_GIT_REPO=$(BASE_DIR)/repos/hadoop-0.18
 HADOOP18_BASE_VERSION=0.18.3
+HADOOP18_SOURCE=hadoop-$(HADOOP18_BASE_VERSION).tar.gz
+HADOOP18_SITE=http://apache.cloudera.com/hadoop/core/hadoop-$(HADOOP18_BASE_VERSION)
+HADOOP18_GIT_REPO=$(BASE_DIR)/repos/hadoop-0.18
+# jdiff workaround... bother.
 HADOOP18_BASE_REF=release-0.18.3-with-jdiff
-HADOOP18_BUILD_REF=cdh-0.18.3
-$(eval $(call gitpackage,hadoop18,HADOOP18))
+HADOOP18_BUILD_REF=cdh-$(HADOOP18_BASE_VERSION)
+$(eval $(call GITPACKAGE,hadoop18,HADOOP18))
 
 # Hadoop 0.20.0-based hadoop package
-HADOOP20_SOURCE=hadoop-0.20.0.tar.gz
-HADOOP20_SITE=http://apache.cloudera.com/hadoop/core/hadoop-0.20.0
-HADOOP20_GIT_REPO=$(BASE_DIR)/repos/hadoop-0.20
 HADOOP20_BASE_VERSION=0.20.0
-HADOOP20_BASE_REF=apache/tags/release-0.20.0
-HADOOP20_BUILD_REF=cdh-0.20.0
-$(eval $(call gitpackage,hadoop20,HADOOP20))
+HADOOP20_SOURCE=hadoop-$(HADOOP20_BASE_VERSION).tar.gz
+HADOOP20_SITE=http://apache.cloudera.com/hadoop/core/hadoop-$(HADOOP20_BASE_VERSION)
+HADOOP20_GIT_REPO=$(BASE_DIR)/repos/hadoop-0.20
+HADOOP20_BASE_REF=apache/tags/release-$(HADOOP20_BASE_VERSION)
+HADOOP20_BUILD_REF=cdh-$(HADOOP20_BASE_VERSION)
+$(eval $(call GITPACKAGE,hadoop20,HADOOP20))
 
 # Pig 0.3.0
 #PIG_SOURCE=pig-0.3.0.tar.gz
@@ -40,4 +51,10 @@ $(eval $(call gitpackage,hadoop20,HADOOP20))
 #PIG_BUILD_REF=cdh-0.18
 #$(eval $(call gitpackage,pig,PIG))
 
-packages: $(TARGETS)
+packages: $(TARGETS) 
+
+help-header:
+	@echo "CDH targets:"
+	@echo "    all (or world)"
+
+package-help: help-header $(TARGETS_HELP)
--- mk/gitpackage.mk
+++ mk/gitpackage.mk
@@ -1,41 +1,73 @@
 # Git Package make function
 # $1 is the target prefix, $2 is the variable prefix
-define gitpackage
-$(2)_BUILD_DIR =   $(BUILD_DIR)/$(1)-build
-$(2)_STAMP_PREP =  $(STAMP_DIR)/$(1)-prep
-$(2)_STAMP_PATCH = $(STAMP_DIR)/$(1)-patch
-$(2)_STAMP_BUILD = $(STAMP_DIR)/$(1)-build
-$(2)_VERSION:= $(shell cd $($(2)_GIT_REPO) && $(BASE_DIR)/tools/branch-tool version)
+define GITPACKAGE
 
+$(2)_BUILD_DIR =   $(BUILD_DIR)/$(1)-build
+
+$(2)_TARGET_PREP  = $(STAMP_DIR)/$(1)-prep
+$(2)_TARGET_PATCH = $(STAMP_DIR)/$(1)-patch
+$(2)_TARGET_BUILD = $(STAMP_DIR)/$(1)-build
+$(2)_TARGET_SAVE  = $(STAMP_DIR)/$(1)-save 
+
+$(2)_FULL_VERSION:= $(shell cd $($(2)_GIT_REPO) && $(BASE_DIR)/tools/branch-tool version)
+$(2)_OUTPUT = $(OUTPUT_DIR)/hadoop-$$($(2)_FULL_VERSION).tar.gz
+
+# Step 1
+# TODO: md5
 $(DL_DIR)/$($2_SOURCE):
 	wget -P $(DL_DIR) --progress bar $($(2)_SITE)/$($(2)_SOURCE)
 
-$$($(2)_STAMP_PREP):
+# Step 2
+$$($(2)_TARGET_PREP):
 	mkdir -p $$($(2)_BUILD_DIR)
 	$(BASE_DIR)/tools/setup-package-build $($(2)_GIT_REPO) $($(2)_BASE_REF) $($(2)_BUILD_REF) $(DL_DIR)/$($(2)_SOURCE) $$($(2)_BUILD_DIR)
-	touch $$($(2)_STAMP_PREP)
+	touch $$@
 
-$$($(2)_STAMP_PATCH):
+# Step 3
+$$($(2)_TARGET_PATCH):
 	$$($(2)_BUILD_DIR)/cloudera/apply-patches $$($(2)_BUILD_DIR) $$($(2)_BUILD_DIR)/cloudera/patches
-	touch $$($(2)_STAMP_PATCH) 
+	touch $$@
 
-$$($(2)_STAMP_BUILD):
+# Step 4
+$$($(2)_TARGET_BUILD):
 	/usr/bin/env JAVA32_HOME=$(JAVA32_HOME) JAVA64_HOME=$(JAVA64_HOME) JAVA5_HOME=$(JAVA5_HOME) FORREST_HOME=$(FORREST_HOME) $$($(2)_BUILD_DIR)/cloudera/do-release-build
-	touch $$($(2)_STAMP_BUILD)
+	touch $$@
 
+# Step 5
+# TODO: don't make this hadoop specific
+$$($(2)_TARGET_SAVE):
+	cp -f $$($(2)_BUILD_DIR)/build/hadoop-$$($(2)_FULL_VERSION).tar.gz $(OUTPUT_DIR)
+	touch $$@
+
+# We download target when the source is not in the download directory
 $(1)-download: $(DL_DIR)/$$($(2)_SOURCE) 
 
-$(1)-prep: $(1)-download $$($(2)_STAMP_PREP)
+# To prep target, we need to download it first
+$(1)-prep: $(1)-download $$($(2)_TARGET_PREP)
 
-$(1)-patch: $(1)-prep $$($(2)_STAMP_PATCH)
+# To patch target, we need to prep it first
+$(1)-patch: $(1)-prep $$($(2)_TARGET_PATCH)
+ 
+# To build target, we need to patch it first
+$(1)-build: $(1)-patch $$($(2)_TARGET_BUILD)
 
+# To save target, we need to build it first
+$(1)-save: $(1)-build $$($(2)_TARGET_SAVE)
+
+$(1): $(1)-save
+
+# Helper targets -version -help etc
 $(1)-version:
-	@echo "$$($(2)_VERSION)"
+	@echo "Base: $$($(2)_BASE_VERSION)"
+	@echo "Full: $$($(2)_FULL_VERSION)"
+
+$(1)-help:
+	@echo "    $(1)"
 
 $(1)-clean: 
 	-rm -f $(STAMP_DIR)/$(1)*
 	-rm -rf $$($(2)_BUILD_DIR)
 
-$(1): $(1)-patch $(STAMP_DIR)/$(1)-build
 TARGETS += $(1) 
+TARGETS_HELP += $(1)-help
 endef
