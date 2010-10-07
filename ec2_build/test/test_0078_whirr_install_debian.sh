db99496365b9a1ce000e95400feeb98cbd0d801b
Tom White <tom@cloudera.com>
2010-10-06 17:01:30 -0700

    Add Whirr to ec2_build.

---
+++ ec2_build/test/test_0078_whirr_install_debian.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+# (c) Copyright 2009 Cloudera, Inc.
+
+set -e
+
+############################## SETUP BUILD ENV ##############################
+
+# Install things needed to build
+export DEBIAN_FRONTEND=noninteractive
+
+export CDH_REPO_HOST="$1"
+export CDH_REPO_VERSION="$2"
+
+# Make sure this script only runs on debian/ubuntu
+if [ ! -e "/etc/debian_version" ]; then
+  echo "This script is made for debian-like OS. Exiting"
+  exit 0
+fi
+
+apt-cache search whirr
+apt-get install -y whirr
+whirr version
+apt-get remove -y whirr
+
+echo "Done."
+exit 0
---
+++ ec2_build/test/test_0088_whirr_install_redhat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+# (c) Copyright 2009 Cloudera, Inc.
+set -e
+
+export CDH_REPO_HOST="$1"
+export CDH_REPO_VERSION="$2"
+
+# Make sure this script only runs on redhat
+if [ ! -e "/etc/redhat-release" ]; then
+  echo "This script is made for redhat-like OS. Exiting"
+  exit 0
+fi
+
+yum search whirr
+yum  -y install whirr
+whirr version
+yum  -y remove whirr
+
+echo "Done."
+exit 0
