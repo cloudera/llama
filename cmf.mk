4f1150c000a0713ecc2104d53264e0ce30293b2c
Philip Langdale <philipl@cloudera.com>
2011-01-28 12:15:13 -0800

    Add json and mk files for doing CMF builds.

---
+++ cmf.mk
@@ -0,0 +1,14 @@
+CDH_VERSION=3
+
+# CMF
+CMF_BASE_VERSION=0.1
+CMF_NAME=cmf
+CMF_PKG_NAME=cmf
+CMF_BASE_REF=master
+CMF_BUILD_REF=cdh-packaging-test
+CMF_TARBALL_DST=cmf-$(CMF_BASE_REF).tar.gz
+CMF_TARBALL_SRC=$(CMF_TARBALL_DST)
+CMF_GIT_REPO=$(REPO_DIR)/cmf/cmf
+CMF_PACKAGE_GIT_REPO=$(REPO_DIR)/cmf/cmf-package
+CMF_SITE=http://git.sf.cloudera.com/index.cgi/cmf.git/snapshot/
+$(eval $(call PACKAGE,cmf,CMF))
