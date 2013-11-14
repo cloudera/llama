1d849a2e7bf9debd1d4cd2ceb0d9940ca48898c1
Alejandro Abdelnur <tucu@cloudera.com>
2013-11-13 21:00:11 -0800

    Adding external hadoop option to minillama

--- mini-llama/src/main/java/com/cloudera/llama/am/MiniLlama.java
+++ mini-llama/src/main/java/com/cloudera/llama/am/MiniLlama.java
@@ -18,36 +18,42 @@
 package com.cloudera.llama.am;
 
 import com.cloudera.llama.am.api.LlamaAM;
+import com.cloudera.llama.am.impl.FastFormat;
 import com.cloudera.llama.am.impl.ParamChecker;
 import com.cloudera.llama.am.yarn.YarnRMLlamaAMConnector;
 import com.cloudera.llama.server.AbstractServer;
 import com.cloudera.llama.server.NodeMapper;
 import com.cloudera.llama.server.ServerConfiguration;
+import com.cloudera.llama.util.CLIParser;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PatternOptionBuilder;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hdfs.DFSClient;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
-import org.apache.hadoop.hdfs.server.datanode.DataNode;
+import org.apache.hadoop.hdfs.protocol.DatanodeInfo;
+import org.apache.hadoop.hdfs.protocol.HdfsConstants;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.authorize.ProxyUsers;
 import org.apache.hadoop.util.ReflectionUtils;
 import org.apache.hadoop.yarn.api.records.NodeId;
+import org.apache.hadoop.yarn.api.records.NodeReport;
+import org.apache.hadoop.yarn.api.records.NodeState;
+import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.server.MiniYARNCluster;
-import org.apache.hadoop.yarn.server.nodemanager.NodeManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -61,111 +67,111 @@ public class MiniLlama {
     SLF4JBridgeHandler.install();
   }
 
-  private static final String NODES_OPT = "hadoopnodes";
-  private static final String NOFORMAT_OPT = "noformat";
-  private static final String WRITE_HDFS_CONFIG_OPT = "writehdfsconf";
+  private static final String HELP_CMD = "help";
+  private static final String MINICLUSTER_CMD = "minicluster";
+  private static final String CLUSTER_CMD = "cluster";
+
+  private static final String NODES = "nodes";
+  private static final String HDFS_NO_FORMAT = "hdfsnoformat";
+  private static final String HDFS_WRITE_CONF = "hdfswriteconf";
+
+  private static CLIParser createParser() {
+    CLIParser parser = new CLIParser("minillama", new String[0]);
+
+    Option nodes = new Option(NODES, true, "number of nodes (default: 1)");
+    nodes.setRequired(false);
+    nodes.setType(PatternOptionBuilder.NUMBER_VALUE);
+    Option hdfsNoFormat = new Option(HDFS_NO_FORMAT, false,
+        "don't format mini HDFS");
+    hdfsNoFormat.setRequired(false);
+    Option hdfsWriteConf = new Option(HDFS_WRITE_CONF, true,
+        "file to write mini HDFS configuration");
+    hdfsWriteConf.setRequired(false);
+
+    //help
+    Options options = new Options();
+    parser.addCommand(HELP_CMD, "",
+        "display usage for all commands or specified command", options, false);
+
+    //minicluster
+    options = new Options();
+    options.addOption(nodes);
+    options.addOption(hdfsNoFormat);
+    options.addOption(hdfsWriteConf);
+    parser.addCommand(MINICLUSTER_CMD, "",
+        "start embedded mini HDFS/Yarn cluster", options, false);
+
+    //cluster
+    options = new Options();
+    parser.addCommand(CLUSTER_CMD, "", "use external HDFS/Yarn cluster",
+        options, false);
+    return parser;
+  }
 
   public static void main(String[] args) throws Exception {
-    CommandLine cli = parseArguments(args);
-    if (cli == null) {
-      return;
-    }
     Configuration conf = new Configuration(false);
     conf.addResource("llama-site.xml");
-    int nodes = intArgument(cli, NODES_OPT, 1);
-    conf = createMiniClusterConf(conf, nodes);
-    final MiniLlama llama = new MiniLlama(conf);
-    llama.skipDfsFormat(cli.hasOption(NOFORMAT_OPT));
-    if (cli.hasOption(WRITE_HDFS_CONFIG_OPT)) {
-      llama.setWriteHadoopConfig(cli.getOptionValue(WRITE_HDFS_CONFIG_OPT));
-    }
-    llama.start();
-    LOG.info("**************************************************************"
-        + "*******************************************************");
-    LOG.info("Mini Llama running with HDFS/Yarn minicluster with {} nodes, " +
-        "HDFS URI: {} Llama URI: {}", nodes,
-        llama.getHadoopConf().get("fs.defaultFS"),
-        llama.getAddressHost() + ":" + llama.getAddressPort());
-    LOG.info("*************************************************************" +
-        "********************************************************");
-    Runtime.getRuntime().addShutdownHook(new Thread("minillama-shutdownhoock") {
-      @Override
-      public void run() {
-        llama.stop();
-      }
-    });
-    synchronized (MiniLlama.class) {
-      MiniLlama.class.wait();
-    }
-  }
 
-  /**
-   * Creates configuration options object.
-   */
-  @SuppressWarnings("static-access")
-  static Options makeOptions() {
-    Options options = new Options();
-    options
-        .addOption(NODES_OPT, true, "How many nodes to use (default 1)")
-        .addOption(NOFORMAT_OPT, false, "Do not format the DFS (default false)")
-        .addOption(WRITE_HDFS_CONFIG_OPT, true, 
-            "Save Hadoop configuration to this XML file.")
-        .addOption(
-            OptionBuilder.withDescription("Prints option help.").create("help"));
-    return options;
-  }
-
-  /**
-   * Parses arguments.
-   *
-   * @param args
-   *          Command-line arguments.
-   * @return a the parsed args on successful parse; null to indicate that the
-   *         program should exit.
-   */
-  static CommandLine parseArguments(String[] args) {
-    Options options = makeOptions();
-    CommandLine cli;
+    CLIParser parser = createParser();
     try {
-      CommandLineParser parser = new GnuParser();
-      cli = parser.parse(options, args);
-    } catch (ParseException e) {
-      LOG.warn("options parsing failed:  " + e.getMessage());
-      new HelpFormatter().printHelp("...", options);
-      return null;
-    }
-
-    if (cli.hasOption("help")) {
-      new HelpFormatter().printHelp("...", options);
-      return null;
-    }
-    if (cli.getArgs().length > 0) {
-      //TODO: check this with casey
-      for (String arg : cli.getArgs()) {
-        System.err.println("Unrecognized option: " + arg);
-        new HelpFormatter().printHelp("...", options);
-        return null;
+      CLIParser.Command command = parser.parse(args);
+      if (command.getName().equals(HELP_CMD)) {
+        parser.showHelp(command.getCommandLine());
+      } else {
+        final MiniLlama llama;
+        if (command.getName().equals(MINICLUSTER_CMD)) {
+          CommandLine cl = command.getCommandLine();
+          int nodes = Integer.parseInt(cl.getOptionValue(NODES, "1"));
+          conf = createMiniLlamaConf(conf, nodes);
+          llama = new MiniLlama(conf);
+          llama.skipDfsFormat(cl.hasOption(HDFS_NO_FORMAT));
+          if (cl.hasOption(HDFS_WRITE_CONF)) {
+            llama.setWriteHadoopConfig(cl.getOptionValue(HDFS_WRITE_CONF));
+          }
+        } else {
+          conf.setBoolean(MINI_USE_EXTERNAL_HADOOP_KEY, true);
+          conf = createMiniLlamaConf(conf, 1); //nodes is ignored
+          llama = new MiniLlama(conf);
+        }
+        llama.start();
+        String clusterType = (command.getName().equals(MINICLUSTER_CMD))
+                             ? "external HDFS/Yarn cluster"
+                             : "embedded HDFS/Yarn mini-cluster";
+        LOG.info("**************************************************************"
+            + "*******************************************************");
+        LOG.info("Mini Llama running with {} with {} nodes, " +
+            "HDFS URI: {} Llama URI: {}", clusterType, llama.getNodes(),
+            llama.getHadoopConf().get("fs.defaultFS"),
+            llama.getAddressHost() + ":" + llama.getAddressPort());
+        LOG.info("*************************************************************" +
+            "********************************************************");
+        Runtime.getRuntime().addShutdownHook(new Thread("minillama-shutdownhoock") {
+          @Override
+          public void run() {
+            llama.stop();
+          }
+        });
+        synchronized (MiniLlama.class) {
+          MiniLlama.class.wait();
+        }
       }
+    } catch (ParseException ex) {
+      System.err.println("Invalid sub-command: " + ex.getMessage());
+      System.err.println();
+      System.err.println(parser.shortHelp());
+      System.exit(1);
+    } catch (Throwable ex) {
+      System.err.println("Error: " + ex.getMessage());
+      ex.printStackTrace(System.err);
+      System.exit(2);
     }
-    return cli;
-  }
 
-  /**
-   * Extracts an integer argument with specified default value.
-   */
-  static int intArgument(CommandLine cli, String argName, int default_) {
-    String o = cli.getOptionValue(argName);
-    if (o == null) {
-      return default_;
-    } else {
-      return Integer.parseInt(o);
-    }
   }
 
   private static ServerConfiguration S_CONF = new AMServerConfiguration(
       new Configuration(false));
 
-  public static Configuration createMiniClusterConf(Configuration conf,
+  public static Configuration createMiniLlamaConf(Configuration conf,
       int nodes) {
     ParamChecker.notNull(conf, "conf");
     ParamChecker.greaterThan(nodes, 0, "nodes");
@@ -179,16 +185,21 @@ public class MiniLlama {
         ServerConfiguration.SERVER_ADMIN_ADDRESS_KEY), "localhost:0");
     conf.setIfUnset(S_CONF.getPropertyName(
         ServerConfiguration.HTTP_ADDRESS_KEY), "localhost:0");
+    conf.setBoolean(YarnConfiguration.RM_SCHEDULER_INCLUDE_PORT_IN_NODE_NAME,
+        true);
     return conf;
   }
 
   public static Configuration createMiniClusterConf(int nodes) {
-    return createMiniClusterConf(new Configuration(false), nodes);
+    return createMiniLlamaConf(new Configuration(false), nodes);
   }
 
   private static final Logger LOG = LoggerFactory.getLogger(MiniLlama.class);
 
-  public static final String MINI_SERVER_CLASS_KEY = 
+  public static final String MINI_USE_EXTERNAL_HADOOP_KEY =
+      "llama.am.server.mini.use.external.hadoop";
+
+  public static final String MINI_SERVER_CLASS_KEY =
       "llama.am.server.mini.server.class";
 
   private static final String MINI_CLUSTER_NODES_KEY =
@@ -202,6 +213,8 @@ public class MiniLlama {
   private MiniDFSCluster miniHdfs;
   private Configuration hadoopConf;
   private MiniYARNCluster miniYarn;
+  private boolean useExternalHadoop;
+  private int nodes;
 
   public MiniLlama(Configuration conf) {
     ParamChecker.notNull(conf, "conf");
@@ -209,6 +222,7 @@ public class MiniLlama {
         LlamaAMServer.class, AbstractServer.class);
     server = ReflectionUtils.newInstance(klass, conf);
     this.conf = server.getConf();
+    useExternalHadoop = conf.getBoolean(MINI_USE_EXTERNAL_HADOOP_KEY, false);
   }
 
   public Configuration getConf() {
@@ -226,20 +240,66 @@ public class MiniLlama {
   public void setWriteHadoopConfig(String writeHdfsConfig) {
     this.writeHdfsConfig = writeHdfsConfig;
   }
-  
+
+  private Map<String, String> getDataNodeNodeManagerMapping(Configuration conf) throws Exception {
+    Map<String, String> map = new HashMap<String, String>();
+    DFSClient dfsClient = new DFSClient(new URI(conf.get("fs.defaultFS")), conf);
+    DatanodeInfo[] DNs = dfsClient.datanodeReport(HdfsConstants.DatanodeReportType.ALL);
+    YarnClient yarnClient = YarnClient.createYarnClient();
+    yarnClient.init(conf);
+    yarnClient.start();
+    List<NodeId> nodeIds = getYarnNodeIds(conf);
+    if (nodeIds.size() != DNs.length) {
+      throw new RuntimeException("Number of DNs and NMs differ, MiniLlama " +
+          "node mapping requires them to be equal at startup");
+    }
+    LOG.info("HDFS/YARN mapping:");
+    for (int i = 0; i < DNs.length; i++) {
+      String key = DNs[i].getXferAddr();
+      NodeId nodeId = nodeIds.get(i);
+      String value = nodeId.getHost() + ":" + nodeId.getPort();
+      map.put(key, value);
+      LOG.info("  DN/NM: " + key + "/" + value);
+    }
+    yarnClient.stop();
+    nodes = map.size();
+    verifySingleHost(map.keySet(), "DataNode");
+    verifySingleHost(map.values(), "NodeManager");
+    return map;
+  }
+
+  private void verifySingleHost(Collection<String> addresses, String nodeType) {
+    String host = null;
+    for (String address : addresses) {
+      String h = address.substring(0, address.indexOf(":"));
+      if (host == null) {
+        host = h;
+      } else if (!host.equals(h)) {
+        throw new RuntimeException(
+            FastFormat.format("Cluster {}s are running in the same host: {}",
+                nodeType, addresses));
+      }
+    }
+  }
+
   public void start() throws Exception {
-    Map<String, String> mapping = startMiniHadoop();
+    if (useExternalHadoop) {
+      hadoopConf = new YarnConfiguration();
+    } else {
+      hadoopConf = startMiniHadoop();
+      if (writeHdfsConfig != null) {
+        FileOutputStream fos = new FileOutputStream(new File(writeHdfsConfig));
+        hadoopConf.writeXml(fos);
+        fos.close();
+      }
+    }
     server.getConf().setClass(S_CONF.getPropertyName(
         ServerConfiguration.NODE_NAME_MAPPING_CLASS_KEY),
         MiniClusterNodeMapper.class, NodeMapper.class);
-    MiniClusterNodeMapper.addMapping(getConf(), mapping);
-    for (Map.Entry entry : miniYarn.getConfig()) {
-      conf.set((String) entry.getKey(), (String) entry.getValue());
-    }
-    if (writeHdfsConfig != null) {
-      FileOutputStream fos = new FileOutputStream(new File(writeHdfsConfig));
-      hadoopConf.writeXml(fos);
-      fos.close();
+    Map<String, String> mapping = getDataNodeNodeManagerMapping(hadoopConf);
+    MiniClusterNodeMapper.addMapping(server.getConf(), mapping);
+    for (Map.Entry entry : hadoopConf) {
+      server.getConf().set((String) entry.getKey(), (String) entry.getValue());
     }
     dataNodes = new ArrayList<String>(mapping.keySet());
     dataNodes = Collections.unmodifiableList(dataNodes);
@@ -248,7 +308,13 @@ public class MiniLlama {
 
   public void stop() {
     server.stop();
-    stopMiniHadoop();
+    if (useExternalHadoop) {
+      stopMiniHadoop();
+    }
+  }
+
+  public int getNodes() {
+    return nodes;
   }
 
   public String getAddressHost() {
@@ -263,7 +329,7 @@ public class MiniLlama {
     return dataNodes;
   }
 
-  private Map<String, String> startMiniHadoop() throws Exception {
+  private Configuration startMiniHadoop() throws Exception {
     int clusterNodes = getConf().getInt(MINI_CLUSTER_NODES_KEY, 1);
     if (System.getProperty(MiniDFSCluster.PROP_TEST_BUILD_DATA) == null) {
       String testBuildData = new File("target").getAbsolutePath();
@@ -301,23 +367,30 @@ public class MiniLlama {
         true);
     miniYarn.init(conf);
     miniYarn.start();
-    hadoopConf = conf;
+    conf = miniYarn.getConfig();
 
     ProxyUsers.refreshSuperUserGroupsConfiguration(conf);
+    return conf;
+  }
 
-    Map<String, String> mapping = new HashMap<String, String>();
-    LOG.info("Nodes:");
-    for (int i = 0; i < clusterNodes; i++) {
-      DataNode dn = miniHdfs.getDataNodes().get(i);
-      String key = dn.getDatanodeId().getXferAddr();
-      NodeManager nm = miniYarn.getNodeManager(i);
-      NodeId nodeId = nm.getNMContext().getNodeId();
-      String value = nodeId.getHost() + ":" + nodeId.getPort();
-      mapping.put(key, value);
-      LOG.info("  DN: " + key);
+  private List<NodeId> getYarnNodeIds(Configuration conf) throws Exception {
+    List<NodeId> list = new ArrayList<NodeId>();
+    if (miniYarn != null) {
+      int clusterNodes = getConf().getInt(MINI_CLUSTER_NODES_KEY, 1);
+      for (int i = 0; i < clusterNodes; i++) {
+        list.add(miniYarn.getNodeManager(i).getNMContext().getNodeId());
+      }
+    } else {
+      YarnClient yarnClient = YarnClient.createYarnClient();
+      yarnClient.init(conf);
+      yarnClient.start();
+      List<NodeReport> nodes = yarnClient.getNodeReports(NodeState.RUNNING);
+      for (int i = 0; i < nodes.size(); i++) {
+        list.add(nodes.get(i).getNodeId());
+      }
+      yarnClient.stop();
     }
-    System.out.println();
-    return mapping;
+    return list;
   }
 
   private void stopMiniHadoop() {
