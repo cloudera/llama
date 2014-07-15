caca78bb74f42f664d9579411965d97e5879bd39
Andrew Bayer <andrew@cloudera.com>
2014-07-15 14:11:45 -0700

    Adding the GPL packages to the C5 trunk build

--- ec2_build/bin/createJenkinsJobs.groovy
+++ ec2_build/bin/createJenkinsJobs.groovy
@@ -8,7 +8,7 @@ import com.cloudera.kitchen.jobdsl.JenkinsDslUtils
 
 def slurper = new JsonSlurper()
 def jenkinsJson = slurper.parseText(readFileFromWorkspace("jenkins_metadata.json"))
-def crepoJson = slurper.parseText(readFileFromWorkspace("${jenkinsJson['short-release']}.json"))
+def crepoJson = slurper.parseText(readFileFromWorkspace("${jenkinsJson['core-prefix']}${jenkinsJson['short-release-base']}.json"))
 
 def phases = []
 
@@ -42,10 +42,13 @@ phases = stepNumbers.collect { s ->
     components.findAll { it.value.step == s }
 }
 
+def gplProjects = components.findAll { it.value.'is-gpl' != null && it.value.'is-gpl' }.collect { it.key }
+gplProjects << "gplextras-parcel"
+
 components.each { component, config ->
-    def downstreamJobs = jenkinsJson.platforms.collect { p ->
-        JenkinsDslUtils.platformToJob(p, config['job-suffix'] ?: "")
-    }
+  def downstreamJobs = jenkinsJson.platforms.collect { p ->
+    JenkinsDslUtils.platformToJob(p, config['job-suffix'] ?: "")
+  }
 
     if (!config.skipBinaryTarball) {
         downstreamJobs << JobDslConstants.TARBALL_DEPLOY_JOB
@@ -64,7 +67,7 @@ components.each { component, config ->
 
         //dissabled(true)
 
-        componentParams(delegate, jenkinsJson.release)
+        componentParams(delegate, "${jenkinsJson.'core-prefix'}${jenkinsJson.'release-base'}")
 
         multiscm {
             cdhGit(delegate, jenkinsJson['cdh-branch'])
@@ -87,16 +90,17 @@ components.each { component, config ->
         }
 
         steps {
-            shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
-            shell(JenkinsDslUtils.constructBuildStep(component, jenkinsJson['short-release'],
-                                                     config['skipRelNotes'] ? true : false, jenkinsJson.java7 ? true : false,
-                                                     config.makefileAsMetadata ? true : false,
-                                                     config.skipMiscBits ? true : false,
-                                                     config.version ?: ""))
+          shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
+          shell(JenkinsDslUtils.constructBuildStep(component, "${jenkinsJson.'core-prefix'}${jenkinsJson['short-release-base']}",
+                  config['skipRelNotes'] ? true : false, jenkinsJson.java7 ? true : false,
+                  config.makefileAsMetadata ? true : false,
+                  config.skipMiscBits ? true : false,
+                  config.version ?: "",
+                  config['is-gpl'] ? true : false))
 
-            componentChildren(delegate, downstreamJobs, component, crepoJson.projects.'parcel-build'?.'track-branch', jenkinsJson.java7)
+            componentChildren(delegate, downstreamJobs, component, jenkinsJson.java7, null)
 
-            conditionalRepoUpdate(delegate, jobPrefix)
+            conditionalRepoUpdate(delegate, jobPrefix, config["is-gpl"] != null ? true : false)
         }
 
         publishers {
@@ -113,54 +117,151 @@ components.each { component, config ->
 }
 
 
-def downstreamParcelJobs = jenkinsJson.platforms.collect { p ->
+if (jenkinsJson.'c5-parcel') {
+  def downstreamParcelJobs = jenkinsJson.platforms.collect { p ->
     JenkinsDslUtils.platformToJob(p, "", true, true)
-}
+  }
 
 // Parcel job
-job {
+  job {
     name JenkinsDslUtils.componentJobName(jobPrefix, "Parcel")
 
     logRotator(-1, 15, -1, -1)
-    
+
     //dissabled(true)
 
-    componentParams(delegate, jenkinsJson.release)
-    
+    componentParams(delegate, "${jenkinsJson.'core-prefix'}${jenkinsJson.'release-base'}")
+
     multiscm {
-        cdhGit(delegate, jenkinsJson['cdh-branch'])
-        packageGit(delegate, pkgGitInfo)
+      cdhGit(delegate, jenkinsJson['cdh-branch'])
+      packageGit(delegate, pkgGitInfo)
     }
-    
+
     label JobDslConstants.COMPONENT_PARENT_LABEL
-    
+
     jdk JobDslConstants.PACKAGING_JOB_JDK
-    
+
     triggers {
-        cron("0 3 * * *")
+      cron("0 23 * * *")
     }
-    
+
     steps {
-        shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
-        shell(JenkinsDslUtils.parcelBuildStep(jenkinsJson['short-release']))
+      shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
+      shell(JenkinsDslUtils.parcelBuildStep("${jenkinsJson['core-prefix']}${jenkinsJson['short-release-base']}"))
 
-        componentChildren(delegate, downstreamParcelJobs, "cdh-parcel", crepoJson.projects.'parcel-build'?.'track-branch', jenkinsJson.java7)
+      componentChildren(delegate, downstreamParcelJobs, "cdh-parcel", jenkinsJson.java7,
+              "origin/${crepoJson.projects.'parcel-build'.'track-branch'}")
 
-        conditionalRepoUpdate(delegate, jobPrefix)
+      conditionalRepoUpdate(delegate, jobPrefix, false)
     }
-    
+
     publishers {
-        archiveArtifacts("output/**/cdh-parcel*/*.tar.gz") // ending comment thingie
-        associatedFiles('/mnt/jenkins-staging/binary-staging/${JOB_NAME}-${BUILD_ID}')
+      archiveArtifacts("output/**/cdh-parcel*/*.tar.gz") // ending comment thingie
+      associatedFiles('/mnt/jenkins-staging/binary-staging/${JOB_NAME}-${BUILD_ID}')
 
-        emailSetup(delegate, ["kitchen-build@cloudera.com"])
+      emailSetup(delegate, ["kitchen-build@cloudera.com"])
     }
-    
+
     wrappers {
-        timeout(120)
+      timeout(120)
     }
-}
+  }
 
+// GPL Parcel job
+  job {
+    name JenkinsDslUtils.componentJobName(jobPrefix, "GPLExtras-Parcel")
+
+    logRotator(-1, 15, -1, -1)
+
+    //dissabled(true)
+
+    componentParams(delegate, "${jenkinsJson.'core-prefix'}${jenkinsJson.'release-base'}")
+
+    multiscm {
+      cdhGit(delegate, jenkinsJson['cdh-branch'])
+      packageGit(delegate, pkgGitInfo)
+    }
+
+    label JobDslConstants.COMPONENT_PARENT_LABEL
+
+    jdk JobDslConstants.PACKAGING_JOB_JDK
+
+    triggers {
+      cron("0 23 * * *")
+    }
+
+    steps {
+      shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
+      shell(JenkinsDslUtils.parcelBuildStep("${jenkinsJson['core-prefix']}${jenkinsJson['short-release-base']}", true))
+
+      componentChildren(delegate, downstreamParcelJobs, "gplextras-parcel", jenkinsJson.java7,
+                        "origin/${crepoJson.projects.'parcel-build'.'track-branch'}".replaceAll("cdh", "gplextras"))
+
+      conditionalRepoUpdate(delegate, jobPrefix, false)
+    }
+
+    publishers {
+      archiveArtifacts("output/**/gplextras-parcel*/*.tar.gz") // ending comment thingie
+      associatedFiles('/mnt/jenkins-staging/binary-staging/${JOB_NAME}-${BUILD_ID}')
+
+      emailSetup(delegate, ["kitchen-build@cloudera.com"])
+    }
+
+    wrappers {
+      timeout(120)
+    }
+  }
+} else {
+  // C4 parcel style
+  def downstreamJobs = jenkinsJson.platforms.collect { p ->
+    JenkinsDslUtils.platformToJob(p, "")
+  }
+
+  job {
+    name JenkinsDslUtils.componentJobName(jobPrefix, "Parcel")
+
+    logRotator(-1, 15, -1, -1)
+
+    //dissabled(true)
+
+    c4ParcelParams(delegate, "${jenkinsJson['core-prefix']}${jenkinsJson.'release-base'}", jenkinsJson.'repo-category', jenkinsJson.'parcel-type')
+
+    multiscm {
+      cdhGit(delegate, jenkinsJson['cdh-branch'])
+      packageGit(delegate, pkgGitInfo)
+    }
+
+    label JobDslConstants.COMPONENT_PARENT_LABEL
+
+    jdk JobDslConstants.PACKAGING_JOB_JDK
+
+    triggers {
+      cron("0 23 * * *")
+    }
+
+    steps {
+      shell(JobDslConstants.SHELL_SCRIPT_CLEAN_CACHES)
+      shell(JenkinsDslUtils.c4ParcelBuildStep("${jenkinsJson['core-prefix']}${jenkinsJson['short-release-base']}", jenkinsJson.'parcel-type'))
+
+      componentChildren(delegate, downstreamJobs, "${jenkinsJson.'parcel-type'.toLowerCase()}-parcel",
+              jenkinsJson.java7, false)
+
+      shell(JenkinsDslUtils.c4ParcelPostBuildStep(jenkinsJson.'parcel-type', jenkinsJson.'repo-category'))
+    }
+
+    publishers {
+      archiveArtifacts("output/**/${jenkinsJson.'parcel-type'.toLowerCase()}-parcel*/*.tar.gz") // ending comment thingie
+      associatedFiles('/mnt/jenkins-staging/binary-staging/${JOB_NAME}-${BUILD_ID}')
+
+      emailSetup(delegate, ["kitchen-build@cloudera.com"])
+    }
+
+    wrappers {
+      timeout(120)
+    }
+
+  }
+}
 
 // Parent POM job
 
@@ -209,72 +310,86 @@ job {
     jdk JobDslConstants.PACKAGING_JOB_JDK
 
     parameters {
-        stringParam("PARENT_BUILD_ID", "", "Build ID of parent job whose artifacts will be added to the repo.")
+      stringParam("PARENT_BUILD_ID", "", "Build ID of parent job whose artifacts will be added to the repo.")
+      stringParam("IS_GPL", "false", "Whether this is updating GPL bits")
     }
 
     repoThrottle(delegate, jobPrefix)
 
     steps {
         shell(JenkinsDslUtils.constructRepoUpdateStep(jenkinsJson['repo-category'],
-                                                      "", jenkinsJson['c5-parcel'],
-                                                      "", jenkinsJson.platforms))
-        shell("""#!/bin/bash
-rm /data/4/repos/cdh5-nightly/parcels/*.parcel
-rm /data/4/repos/cdh5-nightly/parcels/manifest.json
-rsync -av --link-dest=/data/4/repos/cdh5-nightly/parcels/latest/ /data/4/repos/cdh5-nightly/parcels/latest/* /data/4/repos/cdh5-nightly/parcels/
-""")
+                jenkinsJson['gpl-repo-category'],
+                jenkinsJson['c5-parcel'],
+                "${jenkinsJson['gpl-prefix']}${jenkinsJson['release-base']}",
+                jenkinsJson.platforms))
     }
 }
 
 // Full build job
 
 job {
-    name JenkinsDslUtils.componentJobName(jobPrefix, "Full-Build")
-    logRotator(-1, 15, -1, -1)
-    
-    //dissabled(true)
+  name JenkinsDslUtils.componentJobName(jobPrefix, "Full-Build")
+  logRotator(-1, 15, -1, -1)
 
-    scm {
-        cdhGit(delegate, jenkinsJson['cdh-branch'])
+  //dissabled(true)
+
+  scm {
+    cdhGit(delegate, jenkinsJson['cdh-branch'])
+  }
+
+  label JobDslConstants.FULL_AND_REPO_JOBS_LABEL
+
+  jdk JobDslConstants.PACKAGING_JOB_JDK
+
+  triggers {
+    cron("13 1 * * 1,3,6")
+  }
+
+  repoThrottle(delegate, jobPrefix)
+
+  steps {
+    shell(JenkinsDslUtils.firstFullBuildStep("${jenkinsJson['core-prefix']}${jenkinsJson.'release-base'}"))
+    parentCall(delegate, JenkinsDslUtils.componentJobName(jobPrefix, "Parent-POM"), false)
+    phases.each { p ->
+      parentCall(delegate, p.collect { it.value['job-name'] }.join(", "), jenkinsJson.java7)
     }
-
-    label JobDslConstants.FULL_AND_REPO_JOBS_LABEL
-    
-    jdk JobDslConstants.PACKAGING_JOB_JDK
-
-    triggers {
-        cron("13 1 * * 1,3,6")
+    if (jenkinsJson.'c5-parcel') {
+        parentCall(delegate, ["Parcel", "GPLExtras-Parcel"].collect { JenkinsDslUtils.componentJobName(jobPrefix, it) })
     }
-    
-    repoThrottle(delegate, jobPrefix)
+    shell(JenkinsDslUtils.repoGenFullBuildStep(jenkinsJson['repo-category'], jenkinsJson['c5-parcel'],
+            false, jenkinsJson.platforms, gplProjects, false, "${jenkinsJson['core-prefix']}${jenkinsJson.'release-base'}", jenkinsJson['base-repo']))
+    shell(JenkinsDslUtils.repoGenFullBuildStep(jenkinsJson['gpl-repo-category'], jenkinsJson['c5-parcel'],
+            true, jenkinsJson.platforms, gplProjects, true, "${jenkinsJson['gpl-prefix']}${jenkinsJson.'release-base'}", jenkinsJson['base-repo']))
+    if (jenkinsJson.'update-static') {
+//      shell(JenkinsDslUtils.updateStaticRepoFullBuildStep(jenkinsJson['repo-category']))
+      downstreamParameterized {
+        trigger(jobPrefix.toUpperCase() + "-Promote-Repository", "ALWAYS", false) {
+          predefinedProps(['REPO_BUILD_ID': '${JOB_NAME}-${BUILD_ID}',
+                           'REPO_PARENT': "${jenkinsJson['repo-category']}-repos",
+                           'CATEGORY': jenkinsJson['repo-category']])
 
-    steps {
-        shell(JenkinsDslUtils.firstFullBuildStep(jenkinsJson.release))
-        parentCall(delegate, JenkinsDslUtils.componentJobName(jobPrefix, "Parent-POM"), false)
-        phases.each { p ->
-            parentCall(delegate, p.collect { it.value['job-name'] }.join(", "), jenkinsJson.java7)
         }
-        parentCall(delegate, JenkinsDslUtils.componentJobName(jobPrefix, "Parcel"))
-        shell(JenkinsDslUtils.repoGenFullBuildStep(jenkinsJson['repo-category'], jenkinsJson['c5-parcel'],
-                                                   false, jenkinsJson.platforms, [], true, null, jenkinsJson['base-repo']))
-        shell(JenkinsDslUtils.updateStaticRepoFullBuildStep(jenkinsJson['repo-category']))
-        shell("""#!/bin/bash
-rsync -av --link-dest=/data/4/repos/cdh5-nightly/parcels/latest/ /data/4/repos/cdh5-nightly/parcels/latest/* /data/4/repos/cdh5-nightly/parcels/
-""")
-        if (jenkinsJson['update-nightly']) {
-            conditionalSteps {
-                condition { 
-                    status('Success', 'Success')
-                }
-                runner("Fail")
-                downstreamParameterized {
-                    trigger(jobPrefix.toUpperCase() + "-Packaging-Update-Nightly", "ALWAYS", false) { 
+      }
+    }
+
+    if (!jenkinsJson.'c5-parcel') {
+      parentCall(delegate, JenkinsDslUtils.componentJobName(jobPrefix, "Parcel"))
+    }
+
+    if (jenkinsJson['update-nightly']) {
+      conditionalSteps {
+        condition {
+          status('Success', 'Success')
+        }
+        runner("Fail")
+        downstreamParameterized {
+          trigger(jobPrefix.toUpperCase() + "-Packaging-Update-Nightly", "ALWAYS", false) {
                         predefinedProp('PARENT_BUILD_ID', '${JOB_NAME}-${BUILD_ID}')
-                    }
-                }
-            }
+          }
         }
+      }
     }
+  }
 
 }
 
@@ -339,21 +454,42 @@ def componentParams(Object delegate, String release) {
     }
 }
 
-def componentChildren(Object delegate, List<String> jobs, String pkg, String parcelBranch, boolean java7 = true) {
-    return delegate.downstreamParameterized {
-        trigger(jobs.join(", "), "ALWAYS", false,
-                ["buildStepFailure": "FAILURE",
-                 "failure": "FAILURE"]) {
-            predefinedProps(['PARENT_BUILD_ID': '${JOB_NAME}-${BUILD_ID}',
-                             'PACKAGES': pkg,
-                             'CDH_RELEASE': '${RELEASE_CODENAME}',
-                             'JAVA7_BUILD': java7 ? "true" : "",
-			     'PARCEL_BUILD_BRANCH': "origin/${parcelBranch}"])
-        }
-    }
+def c4ParcelParams(Object delegate, String release, String repoCategory, String parcelType) {
+  return delegate.parameters {
+    choiceParam('CHILD_BUILD', ["false", "true"], "When called directly or as a result of polling for changes, "
+            + "this will be no, and the existing repository for this release will be re-used. If this is "
+            + "called automatically from the full all-components build, this will be yes, and all repository "
+            + "management will be handled in the parent job.")
+    stringParam("FULL_PARENT_BUILD_ID", "", "When called directly or as a result of polling for changes, this "
+            + "will be empty and ignored. If called from the full all-components build, this will be set "
+            + "the build id of the parent build, and used for staging information.")
+    stringParam("RELEASE_CODENAME", release, "Release codename - generally will not need to be overridden.")
+    stringParam("PKG_ARCHIVE", "http://repos.jenkins.cloudera.com/${repoCategory}/", "The repo where to pull the packages from to build the parcel.")
+    stringParam("PKG_${parcelType}_VERSION", 1, "Just the version number of the release - i.e., 1 for Search, 4.7.0 for CDH")
+  }
 }
 
-def conditionalRepoUpdate(Object delegate, String jobPrefix) {
+def componentChildren(Object delegate, List<String> jobs, String pkg, boolean java7 = true,
+                      String parcelBranch = null) {
+  def params = ['PARENT_BUILD_ID': '${JOB_NAME}-${BUILD_ID}',
+                'PACKAGES': pkg,
+                'CDH_RELEASE': '${RELEASE_CODENAME}',
+                'JAVA7_BUILD': java7 ? "true" : ""]
+
+  if (parcelBranch != null) {
+    params['PARCEL_BUILD_BRANCH'] = parcelBranch
+  }
+
+  return delegate.downstreamParameterized {
+    trigger(jobs.join(", "), "ALWAYS", false,
+            ["buildStepFailure": "FAILURE",
+             "failure": "FAILURE"]) {
+      predefinedProps(params)
+    }
+  }
+}
+
+def conditionalRepoUpdate(Object delegate, String jobPrefix, boolean isGpl = false) {
     return delegate.conditionalSteps {
         condition {
             stringsMatch('${ENV,var="CHILD_BUILD"}', "false", false)
@@ -361,9 +497,9 @@ def conditionalRepoUpdate(Object delegate, String jobPrefix) {
         runner("Fail")
         downstreamParameterized {
             trigger(JenkinsDslUtils.componentJobName(jobPrefix, "Update-Repos"), "ALWAYS", false) { 
-                predefinedProp('PARENT_BUILD_ID', '${JOB_NAME}-${BUILD_ID}')
+                predefinedProps(['PARENT_BUILD_ID': '${JOB_NAME}-${BUILD_ID}',
+                                 'IS_GPL': "${isGpl}"])
             }
         }
     } 
 }
-
