9d8b2239ce51ed83c3b0bbf48eb03f4d8f9832aa
Anubhav Dhoot <adhoot@cloudera.com>
2015-08-04 11:00:42 -0700

    Fix TestClientNotifier#testRetriesAndMaxFailures to use AtomicInteger instead of volatile Fix TestGangAntiDeadlockLlamaAM#testGangReserveBackoffReReserve  unit test to use mock clock
    Change-Id: I7c5ccd791776045a7b6da9998e0b28ab72c0c592
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7334
    Tested-by: jenkins
    Reviewed-by: Arun Suresh <asuresh@cloudera.com>

---
+++ cloudera/pre_commit_hook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+. /opt/toolchain/toolchain.sh
+
+export JAVA_HOME=$JAVA7_HOME
+export PATH=${JAVA_HOME}/bin:${PATH}
+
+export THRIFT_HOME=/opt/toolchain/thrift-0.9.0
+export PATH=${THRIFT_HOME}/bin:${PATH}
+
+export THRIFT_VERSION=`thrift --version`
+if [ "${THRIFT_VERSION}" != "Thrift version 0.9.0" ];
+then
+  echo "Incorrect ${THRIFT_VERSION}, it should be 0.9.0"
+  exit 1
+fi
+
+rm -rf ~/.m2/repository
+
+mvn clean package -Pdist -Dtar -Dmaven.javadoc.skip=true -DskipTests
